e3daec0b3c556bc7087d3d5ff9a613679746aa89
Naresh Kathad <nareshkathad005@gmail.com>
2017-09-14 09:11:22 +0530

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor(){}
+  model = ''
+  username = ''
+  password = ''
 
-  ngOnInit() {
+  constructor(private http: Http, private router: Router){}
+
+  ngOnInit() {}
+
+  onLogin(form: NgForm) {
+    let data = {
+      'username':form.value.username,
+      'password':form.value.password
+    }
+
+    this.http.post('http://localhost:3000/login',data).map((res: Response) => {
+      console.log('response',Response)
+      this.router.navigateByUrl('/app-home');
+      }).subscribe()
   }
-
 
 }
